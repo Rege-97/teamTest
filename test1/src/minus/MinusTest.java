f3643e0a2f26b110180dee4bd37f5d702a133fda
Whistler95 <sdr020071@naver.com>
2025-05-31 16:18:19 +0900

    add: minus

--- test1/src/minus/MinusTest.java
+++ test1/src/minus/MinusTest.java
@@ -1,8 +1,20 @@
 package minus;
 
+import java.util.Scanner;
+
 public class MinusTest {
 
 	public static void main (String[] args) {
+		
+		Scanner scn = new Scanner(System.in);
+		
+		System.out.print("첫번째 수를 입력 하세요");
+		int a = scn.nextInt();
+		
+		System.out.print("두번째 수를 입력 하세요");
+		int b = scn.nextInt();
+		
+		System.out.println(a+"-"+b+" 는 "+(a-b)+" 입니다");
 		
 	}
 }
