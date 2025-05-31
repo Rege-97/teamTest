783b8b5ebebbf4e33f2f880a7083d593048c2d16
Rege-97 <cogus1027@gmail.com>
2025-05-31 16:17:40 +0900

    feat: add plus

--- test1/src/plus/PlusTest.java
+++ test1/src/plus/PlusTest.java
@@ -1,9 +1,18 @@
 package plus;
 
+import java.util.Scanner;
+
 public class PlusTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("첫 번째 수를 입력하세요 :");
+		int a = sc.nextInt();
+		System.out.print("두 번째 수를 입력하세요 :");
+		int b = sc.nextInt();
+
+		System.out.println(a + " + " + b + " = " + (a + b));
 
 	}
 
